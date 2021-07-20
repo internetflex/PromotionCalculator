17893aa760cefce353130d31af08755067b47b96
Internetflex <internetflex@googlemail.com>
2021-07-21 00:14:36 +0100

    Add Fixed Promotion

---
+++ PromotionCalculator/Promotion.cs
@@ -0,0 +1,9 @@
+﻿namespace PromotionCalculator
+{
+    public class SkuCountPromotion
+    {
+        public SkuCountPromotion(char skuId, uint skuCount, decimal fixedPrice)
+        {
+        }
+    }
+}
---
+++ PromotionCalculatorTests/PromotionTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using PromotionCalculator;
+using Shouldly;
+
+namespace PromotionCalculatorTests
+{
+    [TestFixture]
+    public class PromotionTests
+    {
+        [Test]
+        public void ShouldBeAbleToCreatePromotionForFixedNumberofSKUs()
+        {
+            var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
+            skuCountPromotion.ShouldNotBeNull();
+        }
+    }
+}
