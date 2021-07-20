4b6757e43fab74014190b25a2f61f6739c75d59f
Internetflex <internetflex@googlemail.com>
2021-07-21 00:49:11 +0100

    Add SkuCountPromotion Calculation

--- PromotionCalculator/Promotion.cs
+++ PromotionCalculator/Promotion.cs
@@ -1,9 +1,16 @@
-﻿namespace PromotionCalculator
+﻿using System.Collections.Generic;
+
+namespace PromotionCalculator
 {
     public class SkuCountPromotion
     {
         public SkuCountPromotion(char skuId, uint skuCount, decimal fixedPrice)
         {
+        }
+
+        public decimal Calculate(IEnumerable<SKU> cartItems)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
--- PromotionCalculatorTests/PromotionTests.cs
+++ PromotionCalculatorTests/PromotionTests.cs
@@ -13,5 +13,17 @@ namespace PromotionCalculatorTests
             var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
             skuCountPromotion.ShouldNotBeNull();
         }
+
+        [Test]
+        public void ShouldBeAbleToCalculateSkuCountPromotionTotalForCartItems()
+        {
+            var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
+            var cart = new Cart();
+            cart.Add(new SKU('A') {UnitPrice = 50.0m});
+            cart.Add(new SKU('A') {UnitPrice = 50.0m});
+            cart.Add(new SKU('A') {UnitPrice = 50.0m});
+            var total = skuCountPromotion.Calculate(cart.Items);
+            total.ShouldBe(130.0m);
+        }
     }
 }
