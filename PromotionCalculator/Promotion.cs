2e813bf4776e6809a52aa27451e996efbe51852b
Internetflex <internetflex@googlemail.com>
2021-07-21 01:12:36 +0100

    Add further tests for SkuCountPromotion

--- PromotionCalculator/Promotion.cs
+++ PromotionCalculator/Promotion.cs
@@ -4,13 +4,44 @@ namespace PromotionCalculator
 {
     public class SkuCountPromotion
     {
+        private readonly char _skuId;
+        private readonly uint _skuCount;
+        private readonly decimal _fixedPrice;
+
         public SkuCountPromotion(char skuId, uint skuCount, decimal fixedPrice)
         {
+            _skuId = skuId;
+            _skuCount = skuCount;
+            _fixedPrice = fixedPrice;
         }
 
         public decimal Calculate(IEnumerable<SKU> cartItems)
         {
-            throw new System.NotImplementedException();
+            var total = 0.0m;
+            var idItemCount = 0;
+            var idItemSubTotal = 0.0m;
+
+            foreach (var item in cartItems)
+            {
+                if (item.Id == _skuId)
+                {
+                    idItemCount++;
+                    if (idItemCount == _skuCount)
+                    {
+                        total += _fixedPrice;
+                        idItemSubTotal = 0;
+                        idItemCount = 0;
+                    }
+                    else
+                    {
+                        idItemSubTotal += item.UnitPrice;
+                    }
+                }
+                else
+                    total += item.UnitPrice;
+            }
+
+            return total + idItemSubTotal;
         }
     }
 }
--- PromotionCalculatorTests/PromotionTests.cs
+++ PromotionCalculatorTests/PromotionTests.cs
@@ -25,5 +25,44 @@ namespace PromotionCalculatorTests
             var total = skuCountPromotion.Calculate(cart.Items);
             total.ShouldBe(130.0m);
         }
+
+        [Test]
+        public void ShouldBeAbleToCalculateSkuCountPromotionIfNotEnoughForPromotion()
+        {
+            var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
+            var cart = new Cart();
+            cart.Add(new SKU('A') {UnitPrice = 50.0m});
+            cart.Add(new SKU('A') {UnitPrice = 50.0m});
+            var total = skuCountPromotion.Calculate(cart.Items);
+            total.ShouldBe(100.0m);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCalculateSkuCountPromotionNoIdMatches()
+        {
+            var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
+            var cart = new Cart();
+            cart.Add(new SKU('B') {UnitPrice = 10.0m});
+            cart.Add(new SKU('C') {UnitPrice = 20.0m});
+            cart.Add(new SKU('D') {UnitPrice = 30.0m});
+            cart.Add(new SKU('E') {UnitPrice = 40.0m});
+            var total = skuCountPromotion.Calculate(cart.Items);
+            total.ShouldBe(100.0m);
+        }
+
+        [Test]
+        public void ShouldBeAbleToCalculateSkuCountPromotionWhenAppliesTwice()
+        {
+            var skuCountPromotion = new SkuCountPromotion('A', 3, 130.0m);
+            var cart = new Cart();
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            cart.Add(new SKU('A') { UnitPrice = 50.0m });
+            var total = skuCountPromotion.Calculate(cart.Items);
+            total.ShouldBe(260.0m);
+        }
     }
 }
